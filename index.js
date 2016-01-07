015ff6cad7c32913a6f1589800399b28f557a8bb
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-06 22:02:58 -0800

    functions and writefile run but bmp img is black

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 var fs           = require('fs');
 var fileName = '../bitmapI.bmp';
 
-/*
-1. 10 bytes offset to reach the 4bytes info of where the array starts
-2. In this case, buf.readUIntLE(10) -> 1078 : array starts at 1078 index
-3.
 
-*/
 function transform(){
-  fs.readFileSync(fileName, function(err, data){
+  fs.readFile(fileName, function(err, data){
     if (err){
       throw err;
     }
@@ -17,7 +12,7 @@ function transform(){
       if (buf.toString('utf-8',0,2) !== 'BM'){
         throw new Error('Type of file loaded is not a Bitmap file. Header type read does not match \'BM\''); //https://github.com/imccunn/bmp-transform/blob/master/lib/Bitmap.js
       }
-      var START_PX = buf.readUIntLE(10);
+      var offset = buf.readUIntLE(10);
     }
   });
 }
